b8b0e8f63b8f4037867fa63b6d8b5152000bfe14
zengrg <60810245+zengrg@users.noreply.github.com>
2020-10-22 14:46:04 +0545

    adding project files

---
+++ products/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Product
+from .models import Offer
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'discount')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
